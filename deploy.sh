b63ba67f58c8443a90db2c07df93d0eeee5150fb
MSaidG <msgulberk@gmail.com>
2026-02-17 13:01:01 +0000

    Add a video and input for dynamic grid

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-${CC} main.c -o gles_app $(pkg-config --cflags --libs libdrm egl glesv2) -lm -ldl -Wall
+${CC} main.c -o gles_app $(pkg-config --cflags --libs libdrm egl glesv2 ncurses) -lm -ldl -Wall
 
 scp gles_app root@192.168.10.2:/run/media/ROOTFS-mmcblk1p2/home/videos
