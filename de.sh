5b9a504ca32db3425f5059fe4b47ddcca478ad09
MSaidG <msgulberk@gmail.com>
2026-03-13 07:48:47 +0000

    Change mode selection to directly select 1080p

--- de.sh
+++ de.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-${CC} main.c -g -o main_test_hdmi $(pkg-config --cflags --libs gstreamer-1.0 gstreamer-app-1.0 gstreamer-allocators-1.0 gstreamer-video-1.0 libdrm egl glesv2) -lm -ldl -Wall
+${CC} main.c -g -o main_test_sdi $(pkg-config --cflags --libs gstreamer-1.0 gstreamer-app-1.0 gstreamer-allocators-1.0 gstreamer-video-1.0 libdrm egl glesv2) -lm -ldl -Wall
 
-scp main_test_hdmi main.c petalinux@192.168.10.2:/home/petalinux
+scp main_test_sdi main.c petalinux@192.168.10.2:/home/petalinux
